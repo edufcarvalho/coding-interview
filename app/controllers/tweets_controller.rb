feefe075563d6ad0474910de9071700d9078069b
Eduardo Freitas de Carvalho <eduardo.carvalho@pier.digital>
2025-05-20 13:57:54 -0300

    fix: fix tweets endpoint

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,10 +2,13 @@ class TweetsController < ApplicationController
   def index
     limit = (params[:limit] || 10).to_i
     cursor = params[:cursor] # expects ISO8601 timestamp, e.g., "2024-05-20T12:00:00Z"
+    tweets = Tweet.by_user(params[:user_id])
 
-    tweets = Tweet.by_user(params[:user_id]).order(created_at: :desc).limit(limit)
-
-    tweets = tweets.where('created_at < ?', Time.iso8601(cursor)) if cursor
+    if cursor
+      tweets = tweets.where('created_at < ?', Time.iso8601(cursor)).order(created_at: :desc).limit(limit)
+    else
+      tweets = tweets.order(created_at: :desc).limit(limit)
+    end
 
     next_cursor = tweets.last&.created_at&.iso8601
 
